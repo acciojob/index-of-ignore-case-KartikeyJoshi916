e7de0126785097b123a1456fd4bbe36e0a7ce94b
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-12-30 09:44:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const s1copy=s1.toUpperCase();
+	const s2copy=s2.toUpperCase();
+	for(let i=0;i<s1copy.length;i++){
+		let check=false;
+		if(s1copy[i]==s2copy[0]){
+			for(let j=0;j<s2copy.length;j++){
+				if(s1copy[i+j]!=s2copy[j]){
+					check=false
+					break;
+				}else{
+					check=true;
+				}
+			}
+			if(check===true){
+				return i;
+			}
+		}
+	}
+	return -1;
 }
 
 // Please do not change the code below
